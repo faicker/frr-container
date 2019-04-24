d1da11bd8207da4f134e35408ac455bcc72a113d
Faicker Mo <faicker.mo@gmail.com>
2019-04-24 17:14:29 +0800

    start daemon by conf and add some debug tools
    Signed-off-by: Faicker Mo <faicker.mo@gmail.com>

--- frr/start-frr.sh
+++ frr/start-frr.sh
@@ -2,7 +2,18 @@
 
 progs="staticd zebra"
 for prg in bgpd ospfd ospf6d ripd ripngd babeld isisd bfdd pimd ldpd nhrpd eigrpd sharpd fabricd; do
-    progs=$progs" $prg"
+    enabled=0
+    if [[ -f /etc/frr/daemons ]]; then
+        if grep -q "$prg=yes" /etc/frr/daemons; then
+            enabled=1
+        fi
+    fi
+    if [[ -f /etc/frr/$prg.conf ]]; then
+        enabled=1
+    fi
+    if [[ $enabled -eq 1 ]]; then
+        progs=$progs" $prg"
+    fi
 done
 
 /usr/lib/frr/watchfrr $progs
