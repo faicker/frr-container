2608872fff94a30dc9ab490eaefec20c263c2b14
Faicker Mo <faicker.mo@gmail.com>
2019-07-17 11:34:25 +0800

    startfrr change to service cmd

--- frr/entrypoint.sh
+++ frr/entrypoint.sh
@@ -1,17 +1,11 @@
 #!/usr/bin/env bash
 
-DAEMON_FILE="/etc/frr/daemons"
-
-if [[ ! -f $DAEMON_FILE ]]; then
-    echo "$DAEMON_FILE not exist"
-    exit 1
-fi
-
-progs=""
-for prg in zebra bgpd ospfd ospf6d ripd ripngd babeld isisd bfdd pimd ldpd nhrpd eigrpd sharpd fabricd staticd pbrd; do
-    if grep -q "$prg=yes" $DAEMON_FILE; then
-        progs=$progs" $prg"
+service frr start
+sleep 3
+while sleep 2; do
+    if ps -ef | grep 'watchfrr' | grep -qv grep; then
+        :
+    else
+        exit 1
     fi
 done
-
-/usr/lib/frr/watchfrr $progs
